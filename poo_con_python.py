8c890f38672ee7170af034afe3301dbb45f0052f
jessicarodriguezchim <jessica.g.rodriguez.chim@gmail.com>
2025-01-08 11:29:49 -0600

    creando atributos de la clase

--- poo_con_python.py
+++ poo_con_python.py
@@ -1,3 +1,25 @@
-EstoEsCamelCase
-snake_case_ejemplo
+class personaje: 
+    #Atributos de la clase 
+    nombre = 'Default'
+    fuerza = 0
+    inteligencia= 0
+    defensa = 0
+    vida = 0
+    #Indicar que no se haga nada en este momento
+pass
+#Variable del constructo vacío de la clase
+mi_personaje = personaje()
+mi_personaje.nombre = 'JessicaFighter'
+mi_personaje.fuerza = 30
+mi_personaje.inteligencia = 12
+mi_personaje.defensa = 28
+mi_personaje.vida = 3
+
+print("El nombre del personaje es", mi_personaje.nombre)
+print("La fuerza del personaje es", mi_personaje.fuerza)
+print("La inteligencia del personaje es", mi_personaje.inteligencia)
+print("La defensa del personaje es", mi_personaje.defensa)
+print("La vida del personaje es", mi_personaje.vida)
+
+
 
