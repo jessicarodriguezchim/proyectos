f8c9c82e1c53a0c7ed4979143ec2189fb3a59df7
jessicarodriguezchim <jessica.g.rodriguez.chim@gmail.com>
2025-01-15 11:33:54 -0600

    Creando clase guerrero

--- poo_con_python.py
+++ poo_con_python.py
@@ -7,14 +7,14 @@ class personaje:
     # vida = 0
   #constructor de la clase
     def __init__ (self, nombre, fuerza, inteligencia, defensa, vida): 
-        self.__nombre = nombre
-        self.__fuerza = fuerza
-        self.__inteligencia = inteligencia
-        self.__defensa = defensa
-        self.__vida = vida
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
     
     def imprimir_atributos(self):
-        print(self.__nombre)
+        print(self.nombre)
         print("-Fuerza:" , self.fuerza)
         print("-Inteligencia:" , self.inteligencia)
         print("-Defensa:" , self.defensa)
@@ -31,42 +31,44 @@ class personaje:
     
     def morir(self):
         self.vida = 0
-        print(self.__nombre, "ha muerto")
+        print(self.nombre, "ha muerto")
         #return self.vida <= 0
         
     def dañar(self, enemigo):
-        return max(0, self.fuerza - enemigo.defensa)
+        return self.fuerza - enemigo.defensa
     
     def atacar(self, enemigo):
         daño = self.dañar(enemigo)
-        enemigo.vida = max(0,enemigo.vida - daño)
-        print(self.__nombre, "ha realizado", daño, "puntos de daño a", enemigo.__nombre)
-        print("vida de ", enemigo.__nombre, "es ", enemigo.vida)
+        enemigo.vida = enemigo.vida - daño
+        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
+        print("vida de ", enemigo.nombre, "es ", enemigo.vida)
         
-    def get_fuerza(self):
-        return self.__fuerza
-    
-    def set_fuerza(self, fuerza):
-        if fuerza < 0:
-         print("Error, valor negativo")
-        else:
-            self.__fuerza = fuerza
-    
-    
-    
-#Variable del constructo vacío de la clase
-mi_personaje = personaje("Dante", 1000, 3, 70, 1000)
-#mi_personaje.imprimir_atributos()
-mi_enemigo = personaje("Vergil", 70, 30, 70, 100)
-#mi_personaje.fuerza
-# mi_personaje.fuerza = 0
-# mi_enemigo.imprimir_atributos()
-#mi_personaje.morir()
-print(mi_personaje.get_fuerza())
-print(mi_personaje.set_fuerza(-5))
+class Guerrero(personaje):
+    pass
+tlatuani = Guerrero("Apocalipto", 50, 70, 30, 100)
 
-#mi_personaje.atacar(mi_enemigo)
-#mi_enemigo.imprimir_atributos()
+
+
+#Variable del constructo vacío de la clase
+# mi_personaje = personaje("Dante", 100, 3, 70, 100)
+# mi_personaje.imprimir_atributos()
+# mi_enemigo = personaje("Vergil", 70, 30, 70, 100)
+# mi_personaje.atacar(mi_enemigo)
+# mi_enemigo.imprimir_atributos()
+
+# # Antes del ataque
+# print("Antes del ataque:")
+# mi_personaje.imprimir_atributos()
+# mi_enemigo.imprimir_atributos()
+
+# # Ataque
+# print("\nDurante el ataque:")
+# mi_personaje.atacar(mi_enemigo)
+
+# # Después del ataque
+# print("\nDespués del ataque:")
+# mi_personaje.imprimir_atributos()
+# mi_enemigo.imprimir_atributos()
 
 #print(mi_personaje.dañar(mi_enemigo))
 # mi_personaje.subir_nivel(10, 1, 5)
@@ -74,7 +76,7 @@ print(mi_personaje.set_fuerza(-5))
 # mi_personaje.imprimir_atributos()
 
 
-#mi_personaje.__nombre = 'JessicaFighter'
+#mi_personaje.nombre = 'JessicaFighter'
 #mi_personaje.fuerza = 30
 #mi_personaje.inteligencia = 12
 #mi_personaje.defensa = 28
@@ -82,11 +84,12 @@ print(mi_personaje.set_fuerza(-5))
 
 
 
-# print("El nombre del personaje es", mi_personaje.__nombre)
+# print("El nombre del personaje es", mi_personaje.nombre)
 # print("La fuerza del personaje es", mi_personaje.fuerza)
 # print("La inteligencia del personaje es", mi_personaje.inteligencia)
 # print("La defensa del personaje es", mi_personaje.defensa)
 # print("La vida del personaje es", mi_personaje.vida)
+
 
 
 
